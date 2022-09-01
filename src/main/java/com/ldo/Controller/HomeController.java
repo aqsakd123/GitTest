e37224875434029f8919f0f3847c10d6c56921a4
aqsakd123 <truonghnnsr@gmail.com>
2022-09-01 14:05:51 +0700

    msg first bash commit

---
+++ src/main/java/com/ldo/Controller/HomeController.java
@@ -0,0 +1,37 @@
+package com.ldo.Controller;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import Model.Prize;
+import Service.HomeServiceImplement;
+
+@RestController
+public class HomeController {
+	
+	HomeServiceImplement homeService;
+	
+	@GetMapping("/prize")
+	public List<Prize> getPrizeList() {
+
+		int a = 1;
+		int b = 1;
+		int c = 1;
+		int d = 1;
+		return null;
+	}
+	
+	@GetMapping("/prize/{prizeId}")
+	public Prize getPrize(@PathVariable(name="prizeId") Integer prizeId) {
+		return null;
+	}
+	
+	@PostMapping("/addPrize")
+	public ResponseEntity addPrize(@RequestBody Prize prize) {
+		return homeService.addPrize(prize);
+	}
+	
+	
+}
---
+++ src/test/java/com/ldo/LDOrestAPI/LdOrestApiApplicationTests.java
@@ -0,0 +1,15 @@
+package com.ldo.LDOrestAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LdOrestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+		int a = 0;
+		int b = 0;
+	}
+
+}
